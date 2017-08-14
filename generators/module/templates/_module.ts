d69da66ead601b4ab31fa9f551f606f1c200f4d9
yitimo <635928008@qq.com>
2017-08-14 15:11:40 +0800

    fix default page ref when no need

--- generators/module/templates/_module.ts
+++ generators/module/templates/_module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { DefaultComponent } from './default';
+<%if(needRoute){%>import { CommonModule } from '@angular/common';
+<%}%>import { DefaultComponent } from './default';
 import { <%=ModuleName%>Service } from './<%=moduleName%>.service';
 import { <%=ModuleName%>Component } from './<%=moduleName%>.component';
-<%if(needRoute){%>import { <%=ModuleName%>RoutingModule } from './<%=moduleName%>.route';<%}%>
-
+<%if(needRoute){%>import { <%=ModuleName%>RoutingModule } from './<%=moduleName%>.route';
+<%}%>
 @NgModule({
     declarations: [ <%=ModuleName%>Component<%if(needRoute){%>, DefaultComponent<%}%> ],
     imports: [ CommonModule<%if(needRoute){%>, <%=ModuleName%>RoutingModule<%}%> ],
